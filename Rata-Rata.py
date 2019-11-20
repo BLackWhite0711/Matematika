5d1b7db90430d9068723d54866f93bfaf973d3d0
BLackWhite <43264850+BLackWhite0711@users.noreply.github.com>
2019-11-20 11:22:07 -0500

    BLackWhite
    Matematika

---
+++ Rata-Rata.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+#Dibuat OLeh Noprianto
+#BLackWhiteTeam
+
+import math
+import mechanize
+
+print (" ___ __     ___ _______    ")
+print (" | | \ \    | | |_|  \_\   ")
+print (" | |  \ \   | | |_|   \_\  ")
+print (" | |   \ \  | | |_|    \_\ ")
+print (" | |    \ \_| | |_|    /_/ ")
+print ("|___|    \____| |_|___/_/  ")
+print ("-------------------------- ")
+print ("")
+print ("Mencari Rata-Rata ")
+print ("")
+print (" Rumus Rata-Rata ")
+print (" _")
+print (" X = E xi.fi ")
+print ("        2fi  ")
+print ("")
+a = input ("Masukan xi.fi: ")
+b = input ("Masukan 2fi  : ")
+a = int and float (a)
+b = int and float (b)
+print (" Jumlah Rata-Rata = ", (a/b) )
+print ("")
+print (" TrimaKasih ")
+
+
