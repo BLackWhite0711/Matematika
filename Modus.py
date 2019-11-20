7e996dac07fc1cde6cb1fa540a25d110cabfe302
BLackWhite <43264850+BLackWhite0711@users.noreply.github.com>
2019-11-20 11:23:05 -0500

    BLackWhite

---
+++ Modus.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+#Dibuat OLeh Noprianto
+#BLackWhiteTeam
+
+import math
+import mechanize
+
+print (" ___ __     ___ _______    ")
+print (" | | \ \    | | |_|  \_\   ")
+print (" | |  \ \   | | |_|   \_\  ")
+print (" | |   \ \  | | |_|    \_\ ")
+print (" | |    \ \_| | |_|    /_/ ")
+print ("|___|    \____| |_|___/_/  ")
+print ("-------------------------- ")
+print ("")
+print ("Mencari Modus ")
+print ("")
+print ("    Rumus Modus ")
+print ("")
+print ("Li + P . (   d1 )")
+print ("         ( d1+d2)")
+print ("")
+print ("")
+a = input ("Masukan nilai Li : ")
+b = input ("Masukan Panjang  : ")
+c = input ("Masukan nilai d1 : ")
+d = input ("Masukan nilai d1 : ")
+e = input ("Masukan nilai d2 : ")
+a = int and float (a)
+b = int and float (b)
+c = int and float (c)
+d = int and float (d)
+e = int and float (e)
+print ("Jumlah Modus = ", (b*c)/(d+e)+a )
+print ("")
+print ("TrimaKasih")
+
