bbb8e92d5008c76352f788e4b91811678f6e1c71
BLackWhite <43264850+BLackWhite0711@users.noreply.github.com>
2019-11-20 11:22:36 -0500

    BLackWhite

---
+++ Median.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+#Dibuat OLeh Noprianto
+#BLackWhiteTeam
+
+import math
+import mechanize
+
+print (" ___ __     ___ _______    ")
+print (" | | \ \    | | |_|  \_\   ")
+print (" | |  \ \   | | |_|   \_\  ")
+print (" | |   \ \  | | |_|    \_\ ")
+print (" | |    \ \_| | |_|    /_/ ")
+print ("|___|    \____| |_|___/_/  ")
+print ("-------------------------- ")
+print ("")
+print ("Mencari Median ")
+print ("")
+print ("    Rumus Median ")
+print ("")
+print ("Li + P . ( 1,5n + F )")
+print ("               fi   ")
+print ("")
+print ("")
+a = input ("Masukan nilai Li : ")
+b = input ("Masukan Panjang  : ")
+c = input ("Masukan 1,5n     : ")
+d = input ("Masukan nilai F  : ")
+e = input ("Masukan nilai fi : ")
+a = int and float (a)
+b = int and float (b)
+c = int and float (c)
+d = int and float (d)
+e = int and float (e)
+print ("Jumlah Median = ", b*(c-d)/e+a )
+print ("")
+print ("TrimaKasih")
+
